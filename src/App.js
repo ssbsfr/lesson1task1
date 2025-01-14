93317b4b862cbfc989c54dc8b1a09caefd3b8488
Ruslan <ssbsfr@gmail.com>
2025-01-14 04:40:19 +0300

    с JSX

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import './App.css';
 
 function App() {
   return (
+    // Декларативно: описываем, как должен выглядеть DOM-дерево нашего компонента.
     <div className="App">
       <header className="App-header">
+        {/* Декларативно: указываем, что нужно отобразить изображение с заданными классами и атрибутами */}
         <img src={logo} className="App-logo" alt="logo" />
+
+        {/* Декларативно: описание абзаца с текстом */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        {/* Декларативно: описание ссылки, которая ведёт на React сайт */}
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,8 +24,15 @@ function App() {
         >
           Learn React
         </a>
-        {/* Отображаем текущий год */}
-        <p style={{ marginTop: '1rem' }}>&copy; {new Date().getFullYear()}</p>
+
+        {/*
+          Декларативно: выводим текущий год внутри абзаца.
+          Здесь присутствует императивная часть: вызов new Date().getFullYear() – он выполняется как функция для вычисления значения.
+          Однако результат этого вычисления используется декларативно в JSX.
+        */}
+        <p style={{ marginTop: '1rem' }}>
+          &copy; {new Date().getFullYear()}
+        </p>
       </header>
     </div>
   );
